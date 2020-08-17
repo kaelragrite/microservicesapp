556ca49cd8ddaa059d488e3a1bb91f5bd2cb4d03
kaelgrarite <konstantine.kargareteli@gmail.com>
2020-08-18 00:14:13 +0400

    added ocelot gateway project

---
+++ src/ApiGateway/API.Gateway/Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace API.Gateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) => services.AddOcelot();
+
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            await app.UseOcelot();
+        }
+    }
+}
